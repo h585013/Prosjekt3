8f61def8d7cae84d4f742e26e27d71c3191d4e9d
h586616 <lotte-m97@hotmail.com>
2021-03-19 13:22:50 +0100

    lotte: oppdatert spiller klasse

--- src/no/hvl/dat109/spiller/Spiller.java
+++ src/no/hvl/dat109/spiller/Spiller.java
@@ -2,4 +2,32 @@ package no.hvl.dat109.spiller;
 
 public class Spiller {
 
-}
+	private String navn;
+	 private int poeng;
+
+	 public Spiller(String navn, int poeng) {
+		
+		this.navn = navn;
+		this.poeng = poeng;
+	}
+
+	public String getNavn() {
+		return navn;
+	}
+
+	public void setNavn(String navn) {
+		this.navn = navn;
+	}
+
+	public int getPoeng() {
+		return poeng;
+	}
+//commit test 
+	public void setPoeng(int poeng) {
+		this.poeng = poeng;
+	}
+	 
+	 
+
+
+	}
