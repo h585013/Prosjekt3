7857a78e74015b87ef52d1115f61b484715aa57b
584992 <54098897+584992@users.noreply.github.com>
2021-03-19 13:23:37 +0100

    Endrer litt kode

--- src_test/no/hvl/dat109/tester/RegelTester2.java
+++ src_test/no/hvl/dat109/tester/RegelTester2.java
@@ -2,10 +2,17 @@ package no.hvl.dat109.tester;
 
 import static org.junit.jupiter.api.Assertions.*;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.junit.jupiter.api.Test;
 
 import no.hvl.dat109.regler.Hus;
 import no.hvl.dat109.regler.IRegel;
+import no.hvl.dat109.regler.LitenStraight;
+import no.hvl.dat109.regler.Sjanse;
+import no.hvl.dat109.regler.ToPar;
+import no.hvl.dat109.terningkast.Terning;
 
 class RegelTester2 {
 
@@ -13,12 +20,31 @@ class RegelTester2 {
 	//Tester hus
 
 	IRegel hus = new Hus();
+	IRegel litenStraigth = new LitenStraight();	
+	IRegel sjanse = new Sjanse();
+	IRegel toPar = new ToPar();
 	
-	//Hello
+	List<Integer> terningKast = new ArrayList<Integer>();
+	
+	
+	@Test
+	void testHus() {
+		terningKast.add(2);
+		terningKast.add(2);
+		terningKast.add(5);
+		terningKast.add(5);
+		terningKast.add(5);
+
+		hus.resolve(terningKast);
+		
+		
+		
+		
+	}
 	
 	@Test
 	void testLitenStraigth() {
-		;
+		
 	}
 	
 
