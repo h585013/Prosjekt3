6bf39c8f6c3fdaa0af9654695a380df2c8353a90
h586616 <lotte-m97@hotmail.com>
2021-03-19 15:20:24 +0100

    noen halvferdige tester

--- src_test/no/hvl/dat109/tester/RegelTester3.java
+++ src_test/no/hvl/dat109/tester/RegelTester3.java
@@ -1,6 +1,8 @@
 package no.hvl.dat109.tester;
 
 import org.junit.jupiter.api.Test;
+import no.hvl.dat109.regler.IRegel;
+import no.hvl.dat109.regler.RundeEnTilSeks;
 
 public class RegelTester3 {
 
