33b20010d8fd02182df01e8459c6bea2c80bab00
584992 <54098897+584992@users.noreply.github.com>
2021-03-19 13:13:39 +0100

    Lager Tester

---
+++ src_test/no/hvl/dat109/tester/RegelTester2.java
@@ -0,0 +1,22 @@
+package no.hvl.dat109.tester;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+import no.hvl.dat109.regler.Hus;
+import no.hvl.dat109.regler.IRegel;
+
+class RegelTester2 {
+
+	
+	//Tester hus
+
+	IRegel hus = new Hus();
+	
+	@Test
+	void testHus() {
+		;
+	}
+
+}
