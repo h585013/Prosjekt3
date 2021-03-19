ddf044d49dcb0a899e7c39f90199a96ee06e5204
584992 <54098897+584992@users.noreply.github.com>
2021-03-19 13:34:46 +0100

    k

--- src_test/no/hvl/dat109/tester/RegelTester2.java
+++ src_test/no/hvl/dat109/tester/RegelTester2.java
@@ -1,32 +1,26 @@
 package no.hvl.dat109.tester;
 
 import static org.junit.jupiter.api.Assertions.*;
-
 import java.util.ArrayList;
 import java.util.List;
-
 import org.junit.jupiter.api.Test;
-
 import no.hvl.dat109.regler.Hus;
 import no.hvl.dat109.regler.IRegel;
 import no.hvl.dat109.regler.LitenStraight;
 import no.hvl.dat109.regler.Sjanse;
 import no.hvl.dat109.regler.ToPar;
-import no.hvl.dat109.terningkast.Terning;
 
 class RegelTester2 {
 
-	
-	//Tester hus
+	// Tester hus
 
 	IRegel hus = new Hus();
-	IRegel litenStraigth = new LitenStraight();	
+	IRegel litenStraigth = new LitenStraight();
 	IRegel sjanse = new Sjanse();
 	IRegel toPar = new ToPar();
-	
+
 	List<Integer> terningKast = new ArrayList<Integer>();
-	
-	
+
 	@Test
 	void testHus() {
 		terningKast.add(2);
@@ -35,30 +29,25 @@ class RegelTester2 {
 		terningKast.add(5);
 		terningKast.add(5);
 
-		hus.resolve(terningKast);
-		
-		
-		
-		
+		int ErHus = hus.resolve((ArrayList<Integer>) terningKast);
+		assertEquals(19, ErHus);
+
 	}
-	
+
 	@Test
 	void testLitenStraigth() {
-		
+		terningKast.add(2);
+		terningKast.add(3);
 	}
-	
 
 	@Test
 	void testSjanse() {
 		;
 	}
-	
 
 	@Test
 	void testToPar() {
 		;
 	}
-
-	
 
 }
