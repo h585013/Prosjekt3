6ba010da3a98dbc6bf0b10abbae8cc9a67d05209
584992 <54098897+584992@users.noreply.github.com>
2021-03-19 14:17:32 +0100

    forandringer

--- src_test/no/hvl/dat109/tester/RegelTester2.java
+++ src_test/no/hvl/dat109/tester/RegelTester2.java
@@ -17,7 +17,7 @@ class RegelTester2 {
 	// Tester hus
 
 	IRegel hus = new Hus();
-	IRegel litenStraigth = new LitenStraight();
+	IRegel litenStraight = new LitenStraight();
 	IRegel sjanse = new Sjanse();
 	IRegel toPar = new ToPar();
 
@@ -25,7 +25,12 @@ class RegelTester2 {
 	ArrayList<Integer> ErIkkeHus = new ArrayList<Integer>();
 	ArrayList<Integer> ErLitenStraight = new ArrayList<Integer>();
 	ArrayList<Integer> ErIkkeLitenStraight = new ArrayList<Integer>();
+	ArrayList<Integer> Sjanse = new ArrayList<Integer>();
+	ArrayList<Integer> ErToPar = new ArrayList<Integer>();
+	ArrayList<Integer> ErIkkeToPar = new ArrayList<Integer>();
 
+	
+	
 	@Test
 	void testHus() {
 		ErHus.add(2);
@@ -42,29 +47,60 @@ class RegelTester2 {
 		
 		//Tester at det er et hus
 		int testErHus = hus.resolve(ErHus);
-		assertEquals(19, ErHus);
+		assertEquals(16, testErHus);
 		
 		//Tester at det ikke er et hus
 		int testErIkkeHus = hus.resolve( ErIkkeHus);
-		assertEquals(0, ErIkkeHus);
+		assertEquals(0, testErIkkeHus);
 
 	}
 
 	@Test
 	void testLitenStraigth() {
-		
+		ErLitenStraight.add(1);
+		ErLitenStraight.add(2);
+		ErLitenStraight.add(3);
+		ErLitenStraight.add(4);
 		ErLitenStraight.add(5);
-		ErIkkeLitenStraight.add(5);
+		
+		int TestErLitenStraight = litenStraight.resolve(ErLitenStraight);
+		assertEquals(15, TestErLitenStraight);
+		
+		ErIkkeLitenStraight.add(1);
+		ErIkkeLitenStraight.add(2);
+		ErIkkeLitenStraight.add(3);
+		ErIkkeLitenStraight.add(4);
+		ErIkkeLitenStraight.add(4);
+		
+		int TestErIkkeLitenStraight = litenStraight.resolve(ErIkkeLitenStraight);
+		assertEquals(0, TestErIkkeLitenStraight);		
+		
 	}
 
 	@Test
 	void testSjanse() {
-		;
+		Sjanse.add(2);
+		Sjanse.add(3);
+		Sjanse.add(4);
+		Sjanse.add(3);
+		Sjanse.add(1);
+		
+		int TestSjanse = sjanse.resolve(Sjanse);
+		assertEquals(13, TestSjanse);
+		
 	}
 
+	/**
+	 * Tester at det er to par, samt at det høyeste paret blir valgt
+	 */
 	@Test
 	void testToPar() {
-		;
+		ErToPar.add(4);
+		ErToPar.add(4);
+		ErToPar.add(6);
+		ErToPar.add(6);
+		ErToPar.add(5);
+		
 	}
 
 }
