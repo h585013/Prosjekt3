b2e390817d4d21608ba9b1759a27c8b1894295bb
584992 <54098897+584992@users.noreply.github.com>
2021-04-03 22:18:16 +0200

    Lager Venterom

--- src/no/hvl/dat109/dao/BrukerDAO.java
+++ src/no/hvl/dat109/dao/BrukerDAO.java
@@ -1,11 +1,14 @@
 package no.hvl.dat109.dao;
 
+import java.util.List;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
 import javax.transaction.Transactional;
 
 import no.hvl.dat109.databaseEmmaTest.Bruker;
+import no.hvl.dat109.spiller.Spiller;
 
 @Stateless
 public class BrukerDAO {
@@ -24,22 +27,24 @@ public class BrukerDAO {
 		Bruker b = em.createQuery("select b from Bruker b where epost=" + epost, Bruker.class).getSingleResult();
 		return b == null;
 	}
-	
+
 	public Bruker finnBruker(String brukernavn) {
 		return em.find(Bruker.class, brukernavn);
 	}
-	
+
 	public boolean matcherPassord(Bruker b, String pass) {
 		return b.getPassord().equals(pass);
 	}
-	
-	//burde nok ikke være her
-	
+
+	// burde nok ikke være her
+
 	@Transactional
 	public void leggTilBruker(Bruker b) {
 		em.persist(b);
 	}
 
-	
-	
+	public List<Spiller> hentAlle() {
+		return em.createQuery("select s from Spiller d", Spiller.class).getResultList();
+	}
+
 }
---
+++ src/no/hvl/dat109/servlets/VenteromServlet.java
@@ -0,0 +1,38 @@
+package no.hvl.dat109.servlets;
+
+import java.io.IOException;
+import java.util.List;
+
+import javax.ejb.EJB;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
+import no.hvl.dat109.dao.BrukerDAO;
+import no.hvl.dat109.spiller.Spiller;
+
+/**
+ * Servlet implementation class VenteromServlet
+ */
+@WebServlet("/VenteromServlet")
+public class VenteromServlet extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+	
+	@EJB
+	BrukerDAO dao = new BrukerDAO();
+
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	//Viser spillere
+		List<Spiller> spillere = dao.hentAlle();
+	
+	}
+
+	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		//Reload knapp
+	
+	}
+
+}
