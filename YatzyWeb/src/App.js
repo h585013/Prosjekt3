5fff3e13feae35844ca59d6a057a8ac434960fb0
Emma <ehellandhansen@gmail.com>
2021-04-04 12:42:19 +0200

    Test for å se at den ser endringe ri JS-koden.
    : )

--- YatzyWeb/src/App.js
+++ YatzyWeb/src/App.js
@@ -5,7 +5,7 @@ import React, { Component } from 'react';
 import {BrowserRouter, Link, Route, Switch} from 'react-router-dom';
 import { Container, Grid } from '@material-ui/core';
 
-
+//DETTE ER EN TEST! 
 
 const App = () => {
 
