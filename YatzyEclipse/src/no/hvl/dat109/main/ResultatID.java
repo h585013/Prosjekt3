fa16cf917d598728dcc51afc582fde120eb9588a
584481 <584481@stud.hvl.no>
2021-04-07 17:01:59 +0200

    Resultat ID + resultat klassen -thea

---
+++ YatzyEclipse/src/no/hvl/dat109/main/ResultatID.java
@@ -0,0 +1,16 @@
+package no.hvl.dat109.main;
+
+public class ResultatID {
+	private int spillerID; 
+	private int spillID;
+	
+	public ResultatID(int spillerID, int spillID) {
+		super();
+		this.spillerID = spillerID;
+		this.spillID = spillID;
+	} 
+	
+	
+	
+
+}
