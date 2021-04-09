5586f641e6069d262ea8210eca7456fe6a843c9f
581257 <54100236+581257@users.noreply.github.com>
2021-04-09 15:53:22 +0200

    siden vi ikke bruker spiller så må alt endret til bruker

--- YatzyEclipse/src/no/hvl/dat109/servlets/VenteromServlet.java
+++ YatzyEclipse/src/no/hvl/dat109/servlets/VenteromServlet.java
@@ -26,7 +26,7 @@ public class VenteromServlet extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	
-		List<Spiller> spillere = dao.hentAlle();
+		List<Bruker> spillere = dao.hentAlle();
 		request.setAttribute("spillerListe", spillere);	
 		request.getRequestDispatcher("WEB-INF/jsp/venterom.jsp").forward(request, response);
 	}
