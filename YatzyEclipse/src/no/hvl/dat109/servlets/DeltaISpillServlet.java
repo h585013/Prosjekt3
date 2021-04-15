50d12b347ab367faa84f875c234a5c8bd8cf68d3
h586616 <lotte-m97@hotmail.com>
2021-04-15 13:06:41 +0200

    deltaispill

--- YatzyEclipse/src/no/hvl/dat109/servlets/DeltaISpillServlet.java
+++ YatzyEclipse/src/no/hvl/dat109/servlets/DeltaISpillServlet.java
@@ -1,6 +1,4 @@
 
-public class DeltaISpillServlet {
-
 import java.io.IOException;
 import java.util.List;
 
@@ -14,19 +12,40 @@ import javax.servlet.http.HttpSession;
 
 import no.hvl.dat109.dao.BrukerDAO;
 import no.hvl.dat109.registreringOgLogin.Bruker;
+import no.hvl.dat109.dao.SpillDAO;
+import no.hvl.dat109.spill.Spill;
 
 
 /**
  * Servlet implementation class DeltaISpillServlet
  */
-@WebServlet("/VenteromServlet")
-public class VenteromServlet extends HttpServlet {
+@WebServlet("/DeltaISpillServlet")
+public class DeltaISpillServlet extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 		
+	@EJB
+	BrukerDAO brukerdao = new BrukerDAO();
+	
+	
+	protected void doGet(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		request.getRequestDispatcher("WEB-INF/jsp/deltaispill.jsp").forward(request, response);
+	}
 
-		}
+
+	
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		
+		HttpSession sesjon = request.getSession();
+		
+		
+	// int spillID = SpillDAO.finnSpill()
+	
+		
+	}
 
 	}
 	
 	
-}
+
